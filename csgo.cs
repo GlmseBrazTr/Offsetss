d5a1a6c609a6fb6ddb7cae9bbd92a8c4a571348e
GlmseBrazTr <74064106+GlmseBrazTr@users.noreply.github.com>
2022-03-11 21:36:55 +0300

    Update csgo.cs

--- csgo.cs
+++ csgo.cs
@@ -1,6 +1,6 @@
 using System;
 
-// 2022-03-02 08:26:55.120634500 UTC
+// 2022-03-11 17:34:10.356764400 UTC
 
 namespace hazedumper
 {
@@ -147,7 +147,7 @@ namespace hazedumper
         public const Int32 dwZoomSensitivityRatioPtr = 0xDBF1B0;
         public const Int32 dwbSendPackets = 0xD9542;
         public const Int32 dwppDirect3DDevice9 = 0xA5050;
-        public const Int32 find_hud_element = 0x544F50F0;
+        public const Int32 find_hud_element = 0x549150F0;
         public const Int32 force_update_spectator_glow = 0x3BB80A;
         public const Int32 interface_engine_cvar = 0x3E9EC;
         public const Int32 is_c4_owner = 0x3C8890;
